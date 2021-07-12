d49d7d34f6800f3332f7f613be8e2841542fc292
Isaac Aguirre <isaac.aguirre@du.edu>
2021-07-12 02:25:57 -0600

    update 2

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ function addHtml(member) {
             </div>
         </div>`;
         } else {
-            const officePhone = member.getOfficeNumber();
+            const officePhone = member.getOfficgiteNumber();
             data = `<div class="col-6">
             <div class="card mx-auto mb-3" style="width: 18rem">
             <h5 class="card-header">${name}<br /><br />Manager</h5>
